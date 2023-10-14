9e7339324e48545ebac7d93bf321a35aba39dede
jay chauhan <66429052+jay75chauhan@users.noreply.github.com>
2023-10-14 17:24:31 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask , redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "<p>Hello, jay @@!</p>"
+    
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name   
+
+    
 
 if __name__ == '__main__':
     app.run()
