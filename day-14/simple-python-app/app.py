8b032db7eaa851eb4832c29c5c38b1b71c8606cb
jay chauhan <66429052+jay75chauhan@users.noreply.github.com>
2023-10-14 17:45:52 +0530

    ch

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,16 +1,11 @@
-from flask import Flask , redirect, url_for, request
+from flask import Flask 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<p>Hello, jay @@!</p>"
-    
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name   
+def index():
+    return render_template('index.html')
 
-    
 
 if __name__ == '__main__':
     app.run()
