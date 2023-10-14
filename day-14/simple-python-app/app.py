e5093b0ae2494feac0cf540a87f0652780415546
jay chauhan <66429052+jay75chauhan@users.noreply.github.com>
2023-10-14 18:07:03 +0530

    fixed

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,11 +1,16 @@
-from flask import Flask , render_template
+from flask import Flask 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def hello():
+    return "Hello Flask!"
 
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+ 
 
 if __name__ == "__main__": 
     app.run(host ='0.0.0.0', port = 5000, debug = True) 
