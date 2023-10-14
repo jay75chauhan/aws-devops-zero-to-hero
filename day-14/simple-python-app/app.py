c8105199c8958cc1b3464cfab0e51c373cbf353f
jay chauhan <66429052+jay75chauhan@users.noreply.github.com>
2023-10-14 17:55:00 +0530

    fixed flask

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -7,6 +7,6 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__": 
+    app.run(host ='0.0.0.0', port = 5000, debug = True) 
 
