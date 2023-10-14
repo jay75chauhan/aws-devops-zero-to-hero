d9d987899955c116a0b763e9850859e8cdea89db
jay chauhan <66429052+jay75chauhan@users.noreply.github.com>
2023-10-14 13:12:11 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull jay75chauhan/simple-python-flask-app
 
-# Run the Docker image as a container s
+# Run the Docker image as a container 
 docker run -d -p 5000:5000 jay75chauhan/simple-python-flask-app
